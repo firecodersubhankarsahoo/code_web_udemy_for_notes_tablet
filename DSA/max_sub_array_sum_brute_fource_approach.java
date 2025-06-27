5ecf9ef72519c42a145acde67e046dd0cca6ceaa
subhankar sau <115922995+firecodersubhankarsahoo@users.noreply.github.com>
2025-06-28 01:27:38 +0530

    Update max_sub_array_sum_brute_fource_approach.java

--- DSA/max_sub_array_sum_brute_fource_approach.java
+++ DSA/max_sub_array_sum_brute_fource_approach.java
@@ -4,16 +4,16 @@ import java.util.*;
 public class max_sub_array_sum_brute_fource_approach {
 
     public static  void subarray_max_sum(int[] arr){
-        int max=0;
+        int maxSum=0;
         for(int i=0;i<arr.length;i++){
             int st=i;
             for(int j=i;j<arr.length;j++){
                 int ed=j;
-                int sum=0;
+                int currsum=0;
                 for(int k=st;k<=ed;k++){
-                    sum+=arr[k];
+                    currsum+=arr[k];
                 }
-                max=Math.max(sum,max);
+                maxSum=Math.max(currsum,maxSum);
             }
         }
         System.out.println("the max sum of subarrays is = "+max);
