e589ece3e3bfefadae782f88c6cb368de2d6c691
subhankar <sahoosubhankar98325@gmail.com>
2025-07-06 23:34:25 +0530

    _11_remove_duplicates_from_a_string

---
+++ DSA/recursions/_11_remove_duplicates_from_a_string.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class _11_remove_duplicates_from_a_string {
+    static boolean[]arr = new boolean[26];           // Array to keep track of characters seen
+    static  StringBuilder ans=new StringBuilder();
+    //stringbuilder is used instead of string to avoid immutability issues and for better performance
+    public static void removeDuplicate(String str,int idx){
+        if(idx==str.length()-1){                    // Base case: if we have reached the end of the string
+            System.out.println(ans);
+            return;
+        }
+        char currentChar=str.charAt(idx);
+        if(arr[currentChar-'a']==false){         // Check if the character has not been seen before
+            ans.append(currentChar);
+            arr[currentChar-'a']=true;
+            removeDuplicate(str,idx+1);
+        }else {
+            removeDuplicate(str,idx+1);     //duplicate character found, skip it
+        }
+    }
+    public static void main(String[] args) {
+        String str="abacadaeaf";
+        int idx=0; // Starting index
+        removeDuplicate(str, idx); // Call the recursive function
+    }
+}
