618bc1ef3c5ada37a7fa199486044f7951b11f31
subhankar <sahoosubhankar98325@gmail.com>
2025-09-10 01:13:33 +0530

    _7_Duplicate_Parentheses_Code

--- DSA/stack/_7_Duplicate_Parentheses_Code.java
+++ DSA/stack/_7_Duplicate_Parentheses_Code.java
@@ -6,13 +6,17 @@ public class _7_Duplicate_Parentheses_Code {
             char c=exp.charAt(i);
 
             if(c==')'){                           //when we encounter a closing bracket we will check for the top of the stack and pop until we get the opening bracket
+
                 int count=0;       //to count the number of elements between the brackets
+
                 while (!st.isEmpty() && st.peek()!='('){            //pop until we get the opening bracket
                     count++;                                      // increment count for each element popped
                     st.pop();                                    //pop the element until we get the opening bracket
                 }
+
                 if(count<1){                                   //if count is less than 1 it means there are no elements between the brackets so it is duplicate so return true
                     return true;
+
                 }else {                                     //if count is greater than 1 it means there are elements between the brackets so it is not duplicate .now  pop the opening bracket
                     st.pop();
                 }
@@ -21,6 +25,7 @@ public class _7_Duplicate_Parentheses_Code {
                 st.push(c);                          //if it is not closing bracket push everything to the stack
             }
         }
+
         return false;                   //if we reach here it means there are no duplicate brackets so return false
 
     }
